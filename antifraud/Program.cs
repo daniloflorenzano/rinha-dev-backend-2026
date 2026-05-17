28b7bcfb661e83ed68e09c9d287d87141c8006ef
Danilo Florenzano <daniloflorenzano1@outlook.com>
2026-05-17 13:03:52 -0300

    registra rota de healthcheck vinculada ao warmup do banco

--- antifraud/Program.cs
+++ antifraud/Program.cs
@@ -1,23 +1,28 @@
 using Antifraud;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
 using Npgsql;
 
 var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
 
 var connectionString = Environment.GetEnvironmentVariable("DB_CONNECTION_STRING") ??
     "Username=postgres;Password=senha;Host=localhost;Port=5431;Database=postgres;Pooling=true;MaxPoolSize=15;Connection Lifetime=0;";
 
 var dataSourceBuilder = new NpgsqlDataSourceBuilder(connectionString);
 dataSourceBuilder.UseVector();
-await using var dataSource = dataSourceBuilder.Build();
 
-await DbWarmup.PrepareDatabase(dataSource);
+var dataSource = dataSourceBuilder.Build();
+builder.Services.AddSingleton(dataSource);
 
-app.MapGet("/ready", () =>
-{
-    return Results.Ok();
-});
+var warmupTask = Task.Run(async () => await DbWarmup.PrepareDatabase(dataSource));
+
+builder.Services.AddHealthChecks()
+    .AddCheck("DbWarmup", () => 
+        warmupTask.IsCompletedSuccessfully ? HealthCheckResult.Healthy() : HealthCheckResult.Unhealthy());
+
+var app = builder.Build();
+
+app.MapHealthChecks("/ready");
 
 app.MapPost("/fraud-score", ([FromBody]Request req) =>
 {
